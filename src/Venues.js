86f03bec778a56c33673fab86b8641b84116bab1
Sunjeev Guru <sunjeevgururangan@gmail.com>
2020-07-19 18:46:08 -0400

    maps stuff

--- src/Venues.js
+++ src/Venues.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import Component from 'react';
 import { db } from './firebase';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck'
@@ -11,7 +10,8 @@ import Col from 'react-bootstrap/Col';
 import styled from 'styled-components';
 import Badge from 'react-bootstrap/Badge';
 import * as Icons from 'react-bootstrap-icons';
-
+import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
 const Wrapper = styled.section `
     height:100vh;
     width:100%;
@@ -27,25 +27,57 @@ const VenueSection = styled.section `
     padding-top:30px;
 `;
 
+const LoadingSection = styled.section `
+width: 300px;
+height: 300px;
+position: absolute;
+left: 50%;
+top: 50%; 
+margin-left: -150px;
+margin-top: -150px;
+`;
+
 export class Venues extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            venue_data:[]
+            venue_data:[],
+            coordinates:[],
+            loading: true,
+            centered_lat: '', 
+            centered_long: ''
         };
 
     }  
 
     async componentDidMount() {
+
+        if(this.props.location.state.location === "NYC"){
+            this.setState({centered_lat:'40.730610'});
+            this.setState({centered_long:'-73.935242'});
+        }
+        else if(this.props.location.state.location === "DC"){
+            this.setState({centered_lat:'38.9072'});
+            this.setState({centered_long:'-77.0369'});
+        }
+        else if(this.props.location.state.location === "LA"){
+            this.setState({centered_lat:'34.0522'});
+            this.setState({centered_long:'-118.2437'});
+        }
+
         let venues = [];
+        let coor = [];
+
         let venuesRef = db.collection('venues')
         let locationRef = await venuesRef.where('location', '==', this.props.location.state.location).orderBy('name').get();
         for (const venue of locationRef.docs){
             let name = venue.get('name');
             let location = venue.get('location');
             let maxcap = venue.get('maxcap');
-            let cuisines = venue.get('cuisines')
+            let cuisines = venue.get('cuisines');
+            let lat = venue.get('address').latitude;
+            let long = venue.get('address').longitude;
             venues.push({
                 id: venue.id,
                 n: name,
@@ -54,33 +86,53 @@ export class Venues extends React.Component{
                 food_type: cuisines
             });
 
-            this.setState({venue_data: venues});
+            coor.push({
+                id: venue.id,
+                latitude: lat,
+                longitude: long
+            });
+            
         }
+        this.setState({venue_data: venues});
+        this.setState({coordinates: coor});
+        console.log(this.state.coordinates)
 
-        console.log(this.state.venue_data);
+        this.setState({loading: false});
       }
        
 
     
 
     render(){
-       console.log(this.state.venue_data);
+        if(this.state.loading == true){
+            console.log('loading');
+            return (
+                <LoadingSection>
+                    <Alert variant='light'>
+                    <Spinner animation="border" variant="info" />
+                    <h4>Searching for available venues...</h4>
+                    </Alert>
+                </LoadingSection>
+            );
+        }
+        else if(this.state.loading == false){
+            console.log('loaded');
             return (
                 <Wrapper>
                       <Row>
-                        <Col>
+                        <Col md='6'>
                             <MapSection>
-                                <MapView/>
+                            <MapView centered_lat = {this.state.centered_lat} centered_long = {this.state.centered_long} coordinateData = {this.state.coordinates}/>
                             </MapSection>
                         </Col>
 
                         <Col>
                         <Container className="overflow-auto">
                           <VenueSection>
-                            <CardDeck className='mr-0'>
-                                <Row className='p-3'>
+                            <CardDeck>
+                                <Row className='p-6'>
                                 {this.state.venue_data.map((venue,index) => (
-                                    <Col md="4">
+                                    <Col md="6">
                                     <br />
                                 <Card className = "shadow p-3 mb-5 bg-white rounded" key = {venue.id} style={{width: '18rem' }}>
                                     <Card.Img variant="top" src="https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2767&q=80" />
@@ -106,7 +158,8 @@ export class Venues extends React.Component{
                         </Col>
                         </Row>
                 </Wrapper>
-                );
+                                );
+        }
         }
 }
 
--- src/components/MapView.js
+++ src/components/MapView.js
@@ -2,18 +2,32 @@ import React from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapView extends React.Component{
+
     constructor(props){
         super(props);
-
-    }  
+        this.state = ({
+            markers: []
+        });
+        
+    }
+    
+    componentDidMount(){
+        this.setState({markers:this.props.coordinateData})
+        console.log(this.props.coordinateData);
+        console.log(this.state.markers);
+    }
 
     render(){
             return (
                 <div>
-                    <Map google={this.props.google} zoom={14}>
+                    <Map google={this.props.google} zoom={12} center= {{lat: this.props.centered_lat, lng: this.props.centered_long}}>
  
-                        <Marker onClick={this.onMarkerClick}
-                            name={'Current location'} />
+                    { this.props.coordinateData.map(geopoint =>
+                            <Marker
+                                key={geopoint.id}
+                                position={{ lat: geopoint.latitude, lng: geopoint.longitude }}
+                            />
+                        )} 
 
                     <InfoWindow onClose={this.onInfoWindowClose}>
                      <div>
